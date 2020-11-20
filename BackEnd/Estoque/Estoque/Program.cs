c0436100c581b92e5405ccc48a9712fb8476f9d1
lipex90 <phillipemarques45@gmail.com>
2020-11-20 15:01:54 -0300

    criei a pasta de estoque de controle

---
+++ BackEnd/Estoque/Estoque/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Agora temos que fazer a parte de back end");
+        }
+    }
+}
