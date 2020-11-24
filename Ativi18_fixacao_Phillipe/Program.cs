e6e40c67b0c8feac00758f42e1d43951a96a1d6d
lipex90 <phillipemarques45@gmail.com>
2020-11-24 12:59:28 -0300

    Atividade fixacao 18

---
+++ Ativi18_fixacao_Phillipe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ativi18_fixacao_Phillipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> x = null;
+            long? y = null;
+            ushort? z = null;
+            double? t = null;
+            x = 34;
+            y = 65000000;
+            z = 500;
+            t = 7.50;
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.HasValue);
+            Console.WriteLine(z.Value) ;
+        }
+    }
+}
